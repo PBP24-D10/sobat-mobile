28d92a80e7866a149617e96a578872f319398f42
Priyapta <priaptanaufal@gmail.com>
2024-12-02 15:25:24 +0700

    Project Initiate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sobat_mobile/daftar_favorite/screens/daftar_favorite.dart';
 import 'package:sobat_mobile/homepage.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const daftarFavorite(),
     );
   }
 }
